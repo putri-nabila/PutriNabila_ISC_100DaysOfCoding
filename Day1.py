c46abef9d23de2fbae6794bca6329ed5bad31c0b
putri-nabila <ptrinbila29@gmail.com>
2023-10-09 21:27:47 +0800

    Day 1

---
+++ Day1.py
@@ -0,0 +1,13 @@
+#inisialisasi semua variabel
+nama = "Putri Nabila"
+nim = "D0221017"
+angkatan = "2021"
+alamat = "Campalagian"
+prodi = "Informatika"
+
+#menampilkan semua variabel
+print("Nama     : " + nama)
+print("NIM      : " + nim)
+print("Angkatan : " + angkatan)
+print("Alamat   : " + alamat)
+print("Prodi    : " + prodi)
