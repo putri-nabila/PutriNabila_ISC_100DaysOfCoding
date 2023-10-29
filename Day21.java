6527279584eec95229c74c4c567d990d02ff3865
putri-nabila <ptrinbila29@gmail.com>
2023-10-29 21:13:11 +0800

    Mengurutkan bilangan terkecil hingga terbesar menggunakan perulangan

---
+++ Day21.java
@@ -0,0 +1,9 @@
+package days_of_coding;
+
+public class Day21 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Urutan bilangan tekecil hingga terbesar  : " + i);//dimulai dari 1 hinggan 100 yang keseluruhan berjumlah 100
+        }
+    }
+}
