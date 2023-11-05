78b101f75ca720cb1b43fa286f5b132ac7044065
putri-nabila <ptrinbila29@gmail.com>
2023-11-05 20:22:57 +0800

    Perulangan sederhana

---
+++ Day28.java
@@ -0,0 +1,9 @@
+package days_of_coding;
+
+public class Day28 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 1380; i++) {
+            System.out.println("Besok ada tes, semoga lolos!!!");
+        }
+    }
+}
