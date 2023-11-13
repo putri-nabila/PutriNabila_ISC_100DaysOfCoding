76af5396b16e392a2ac2851a314afda8444614dd
putri-nabila <ptrinbila29@gmail.com>
2023-11-13 22:03:07 +0800

    Array

--- Day35.java
+++ Day35.java
@@ -2,6 +2,18 @@ package days_of_coding;
 
 public class Day35 {
     public static void main(String[] args) {
-        
+        String[] anime = {"One Piece", "Kimetsu no Yaiba", "Naruto", "Jujutsu Kaisen", "Haikyu", "Dr. Stone", "Attack On Titan", 
+            "Hunter X Hunter", "Bungou Stray Dogs", "Boku no Hero Academia"};//membuat  list anime
+        //menampilkan list anime berdasarkan indeks yang dimulai dari 0
+        System.out.println(anime[0]);
+        System.out.println(anime[1]);
+        System.out.println(anime[2]);
+        System.out.println(anime[3]);
+        System.out.println(anime[4]);
+        System.out.println(anime[5]);
+        System.out.println(anime[6]);
+        System.out.println(anime[7]);
+        System.out.println(anime[8]);
+        System.out.println(anime[9]);
     }
 }
