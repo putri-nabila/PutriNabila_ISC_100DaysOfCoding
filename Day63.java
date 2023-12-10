1b311476d40110f3aa9990a6780e9e4f7a39307e
putri-nabila <ptrinbila29@gmail.com>
2023-12-11 00:53:53 +0800

    array multidimensi

---
+++ Day63.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package days_of_coding;
+
+/**
+ *
+ * @author acer
+ */
+public class Day63 {
+    public static void main(String[] args) {
+         int [] vektorA = {2, 1, 5};
+        int [] vektorB = {5, 4, 2};
+        
+        
+        for (int i = 0; i < vektorA.length; i++) {
+            System.out.println(vektorA[i] + vektorB[i]+" ");
+        }
+        System.out.println();
+    }
+}
