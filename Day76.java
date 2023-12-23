79e9d653d6a694ca708f5535b8fa0dea7c9f9e27
putri-nabila <ptrinbila29@gmail.com>
2023-12-23 22:05:25 +0800

    menggunakan queue dengan menghapus dan menampilkannya

--- Day76.java
+++ Day76.java
@@ -3,11 +3,12 @@ package days_of_coding;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Day76 {
+public class Day76 {//di day 76 saya mengcommit terkait penggunaan queue dengan menghapus dan menampilkannya
     public static void main(String[] args) {
-        //membuat object bertipe String bernama queue yang di implementasikan di class LinkedList
+        //hal pertama tentu saja membuat objek bertipe String bernama queue yang di implementasikan di class LinkedList
         Queue<String> queue = new LinkedList<>();
-        //program untuk menambahkan elemen2 ke dalam LinkedList
+        
+        //kemudian membuat program untuk menambahkan elemen2 ke dalam LinkedList
         queue.add("Kun");
         queue.add("Ten");
         queue.add("Winwin");
@@ -16,11 +17,11 @@ public class Day76 {
         queue.add("Hendery");
         queue.add("Yangyang");
 
-        //Program untuk menampilkan elemen-elemen yang telah di tambahkan ke dalam likedList
+        //lalu berikut adalah perintah untuk menampilkan elemen-elemen yang telah di tambahkan ke dalam array sebelumnya
         System.out.println("Member WayV: " + queue);
         System.out.println("--------------------------------------------------------------------");
 
-        // program untuk melihat apakah elemen ada dalam antrian atau tidak
+        //setelah itu melihat apakah elemen ada dalam antrian atau tidak
         if (queue.contains("Lucas")) {
             System.out.println("Lucas ditemukan dalam antrian.");
         } else {
@@ -30,15 +31,16 @@ public class Day76 {
         System.out.println("--------------------------------------------------------------------");
 
         //untuk melihat elemen yang berada pada posisi paling depan kita menggunakan "peek"
-        String head = queue.peek();
+        String head = queue.peek();//digunakan untuk melihat elemen yang berada pada posisi paling depan, tanpa harus menghapusnya
         System.out.println("Nama member terdepan dalam antrian: " + head);
         System.out.println("--------------------------------------------------------------------");
 
         //menghapus elemen terdepan
-        String removedHead = queue.remove();
+        String removedHead = queue.remove();//untuk mengapus element paling depan
         System.out.println("Nama member yang akan dihapus: " + removedHead);
         System.out.println("--------------------------------------------------------------------");
-
+        
+        //terakhir menampilkan nama-nama member setelah dihapus
         System.out.println("Nama member setelah menghapus salah satu member: " + queue);
     }
 }
