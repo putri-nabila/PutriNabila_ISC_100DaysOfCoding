5661e7456b64f2be5d73317b514f219a1d977da0
putri-nabila <ptrinbila29@gmail.com>
2023-12-27 22:06:25 +0800

    method sederhana

---
+++ Day80.java
@@ -0,0 +1,16 @@
+package days_of_coding;
+
+public class Day80 {//di day 80 ini saya mengcommit terkait method sederhana
+    //hal pertama yaitu membuat program method, dengan nama nabila
+    static void nabila(){//mendefinisikan metode biodata yang tidak mengembalikan nilai (void) dan bersifat statis (static).
+        //setelah itu mengisi atau membuat apa yang ingin kira tampilkan 
+        System.out.println("Hai, perkenalkan saya putri nabila biasa dipanggil nabila. Saya salah satu mahasiswi aktif di Universitas Sulawesi Barat");
+        System.out.println("Umur saya 20 tahun. Alamat rumah saya di Campalagian, tepatnya di desa Lampoko.");
+        System.out.println("Saya menyukai beberapa hal, salah satunya yaiitu saya sangat menyukai nonton anime.");
+        System.out.println("Saya tidak bisa memilih anime favorit saya, karena saya menyukai semua anime yang sudah saya nonton.");
+        System.out.println("Karena tidak ada yang mengecewakan.a");
+    }
+    public static void main(String[] args) {//kemudian disini menndefinisikan metode utama.
+        nabila();//dan memanggil method utama
+    }
+}
